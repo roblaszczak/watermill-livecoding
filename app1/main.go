f19c9082cff727d6844c5bf47b04f9a0a164ac27
Robert Laszczak <roblaszczak@gmail.com>
2024-11-10 17:33:02 +0100

    added solution

--- app1/main.go
+++ app1/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"math/rand"
@@ -12,6 +13,10 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-kafka/v3/pkg/kafka"
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/google/uuid"
 	"github.com/lmittmann/tint"
 )
@@ -23,6 +28,15 @@ type BookRoomRequest struct {
 
 type RoomBookingHandler struct {
 	payments PaymentsProvider
+
+	eventBus *cqrs.EventBus
+}
+
+type RoomBooked struct {
+	BookingID   string `json:"booking_id"`
+	RoomID      string `json:"room_id"`
+	GuestsCount int    `json:"guests_count"`
+	Price       int    `json:"price"`
 }
 
 func (h RoomBookingHandler) Handler(writer http.ResponseWriter, request *http.Request) {
@@ -46,9 +60,16 @@ func (h RoomBookingHandler) Handler(writer http.ResponseWriter, request *http.Re
 	bookingID := uuid.NewString()
 	roomPrice := 42 * req.GuestsCount
 
-	err = h.payments.TakePayment(bookingID, roomPrice)
+	rb := RoomBooked{
+		BookingID:   bookingID,
+		RoomID:      req.RoomID,
+		GuestsCount: req.GuestsCount,
+		Price:       roomPrice,
+	}
+
+	err = h.eventBus.Publish(request.Context(), rb)
 	if err != nil {
-		slog.With("err", err).Error("Failed to take payment")
+		slog.With("err", err).Error("Failed to publish room booked event")
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -74,6 +95,29 @@ func (p PaymentsProvider) TakePayment(bookingID string, amount int) error {
 	return nil
 }
 
+type PaymentsHandler struct {
+	paymentsProvider PaymentsProvider
+	eventBus         *cqrs.EventBus
+}
+
+type PaymentTaken struct {
+	BookingID string `json:"booking_id"`
+	RoomID    string `json:"room_id"`
+	Price     int    `json:"price"`
+}
+
+func (p PaymentsHandler) Handler(ctx context.Context, rb *RoomBooked) (err error) {
+	if err := p.paymentsProvider.TakePayment(rb.BookingID, rb.Price); err != nil {
+		return err
+	}
+
+	return p.eventBus.Publish(ctx, PaymentTaken{
+		BookingID: rb.BookingID,
+		RoomID:    rb.RoomID,
+		Price:     rb.Price,
+	})
+}
+
 func main() {
 	slog.SetDefault(slog.New(
 		tint.NewHandler(os.Stderr, &tint.Options{
@@ -82,10 +126,86 @@ func main() {
 		}),
 	))
 
+	watermillLogger := watermill.NewSlogLoggerWithLevelMapping(
+		slog.With("watermill", true),
+		map[slog.Level]slog.Level{
+			slog.LevelInfo: slog.LevelDebug,
+		},
+	)
+
+	publisher, err := kafka.NewPublisher(
+		kafka.PublisherConfig{
+			Brokers:   []string{"kafka:9092"},
+			Marshaler: kafka.DefaultMarshaler{},
+		},
+		watermillLogger,
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	slog.Info("Starting app")
+
+	router := message.NewDefaultRouter(watermillLogger)
+
+	marshaler := cqrs.JSONMarshaler{
+		GenerateName: cqrs.StructName,
+	}
+
+	eventBus, err := cqrs.NewEventBusWithConfig(publisher, cqrs.EventBusConfig{
+		GeneratePublishTopic: func(params cqrs.GenerateEventPublishTopicParams) (string, error) {
+			return params.EventName, nil
+		},
+		Marshaler: marshaler,
+		Logger:    watermillLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	eventProcessor, err := cqrs.NewEventProcessorWithConfig(router, cqrs.EventProcessorConfig{
+		GenerateSubscribeTopic: func(params cqrs.EventProcessorGenerateSubscribeTopicParams) (string, error) {
+			return params.EventName, nil
+		},
+		SubscriberConstructor: func(params cqrs.EventProcessorSubscriberConstructorParams) (message.Subscriber, error) {
+			return kafka.NewSubscriber(
+				kafka.SubscriberConfig{
+					Brokers:       []string{"kafka:9092"},
+					ConsumerGroup: params.HandlerName,
+					Unmarshaler:   kafka.DefaultMarshaler{},
+				},
+				watermillLogger,
+			)
+		},
+		Marshaler: marshaler,
+		Logger:    watermillLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
 
 	h := RoomBookingHandler{
 		payments: PaymentsProvider{},
+		eventBus: eventBus,
+	}
+
+	paymentsHandler := PaymentsHandler{
+		eventBus: eventBus,
+	}
+
+	err = eventProcessor.AddHandlers(
+		cqrs.NewEventHandler("payments", paymentsHandler.Handler),
+		cqrs.NewEventHandler("payments_report", func(ctx context.Context, event *PaymentTaken) error {
+			fmt.Printf("Reporting payment taken: %#v\n", event)
+			return nil
+		}),
+		cqrs.NewEventHandler("payments_report_v2", func(ctx context.Context, event *PaymentTaken) error {
+			fmt.Printf("Reporting payment taken (v2): %#v\n", event)
+			return nil
+		}),
+	)
+	if err != nil {
+		panic(err)
 	}
 
 	http.HandleFunc("POST /book", h.Handler)
@@ -99,7 +219,19 @@ func main() {
 		cancel()
 	}()
 
+	go func() {
+		err := router.Run(context.Background())
+		if err != nil {
+			slog.With("err", err).Error("Failed to start watermill router")
+		}
+	}()
+
 	runHTTP(ctx)
+
+	err = router.Close()
+	if err != nil {
+		slog.With("err", err).Warn("Failed to close Watermill router")
+	}
 }
 
 func runHTTP(ctx context.Context) {
